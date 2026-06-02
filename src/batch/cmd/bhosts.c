5c6a3fa632f1a52fd46ec91ef15838d09c2e84a6
LavaLite-Compute <lavalite@lavalite-compute.io>
2026-06-02 19:27:55 -0400

    Fix bhosts display units MB for memory TB for storage.

--- src/batch/cmd/bhosts.c
+++ src/batch/cmd/bhosts.c
@@ -33,19 +33,35 @@ static const char *host_state_str(int32_t status)
 
 static int imax(int a, int b)
 {
-    return a > b ? a : b;
+    if (a > b)
+        return a;
+    return b;
 }
 
 static int ndigits(int32_t n)
 {
+    int d;
+
     if (n <= 0)
         return 1;
-    int d = 0;
+    d = 0;
     while (n > 0) {
         d++;
         n /= 10;
     }
     return d;
+}
+
+/* memory: always MB */
+static void fmt_mem(uint64_t mb, char *buf, size_t len)
+{
+    snprintf(buf, len, "%lu", (unsigned long)mb);
+}
+
+/* storage: always TB */
+static void fmt_stor(uint64_t mb, char *buf, size_t len)
+{
+    snprintf(buf, len, "%lu", (unsigned long)(mb / (1024 * 1024)));
 }
 
 struct col_widths {
@@ -65,61 +81,52 @@ struct col_widths {
     int susp;
 };
 
-#define FMT_BUF_LEN 16
-
-static void fmt_mb(uint64_t mb, char *buf, size_t len)
-{
-    if (mb >= (uint64_t) 1024 * 1024)
-        snprintf(buf, len, "%luT", (unsigned long) (mb / (1024 * 1024)));
-    else if (mb >= 1024)
-        snprintf(buf, len, "%luG", (unsigned long) (mb / 1024));
-    else
-        snprintf(buf, len, "%luM", (unsigned long) mb);
-}
+#define FMT_BUF_LEN 32
 
 static void compute_widths(struct host_info *h, int n, struct col_widths *w)
 {
     char tmp[FMT_BUF_LEN];
 
-    w->name = strlen("HOST_NAME");
-    w->state = strlen("STATE");
-    w->max = strlen("MAX");
-    w->total_cpu = strlen("NCPU");
-    w->used_cpu = strlen("USED_CPU");
-    w->mem = strlen("MEM");
-    w->used_mem = strlen("USED_MEM");
-    w->storage = strlen("STOR");
-    w->used_storage = strlen("USED_STOR");
-    w->total_gpu = strlen("NGPU");
-    w->used_gpu = strlen("USED_GPU");
-    w->njobs = strlen("NJOBS");
-    w->run = strlen("RUN");
-    w->susp = strlen("SUSP");
+    w->name         = strlen("HOST_NAME");
+    w->state        = strlen("STATE");
+    w->max          = strlen("MAX");
+    w->total_cpu    = strlen("NCPU");
+    w->used_cpu     = strlen("USED_CPU");
+    w->mem          = strlen("MEM_MB");
+    w->used_mem     = strlen("USED_MB");
+    w->storage      = strlen("STOR_TB");
+    w->used_storage = strlen("USED_TB");
+    w->total_gpu    = strlen("NGPU");
+    w->used_gpu     = strlen("USED_GPU");
+    w->njobs        = strlen("NJOBS");
+    w->run          = strlen("RUN");
+    w->susp         = strlen("SUSP");
 
     for (int i = 0; i < n; i++) {
-        uint64_t used_mem = h[i].total_mem_mb - h[i].free_mem_mb;
+        uint64_t used_mem  = h[i].total_mem_mb - h[i].free_mem_mb;
         uint64_t used_stor = h[i].total_storage_mb - h[i].free_storage_mb;
-        int32_t used_cpu = h[i].total_cpu - h[i].free_cpu;
-        int32_t used_gpu = h[i].total_gpu - h[i].free_gpu;
+        int32_t  used_cpu  = h[i].total_cpu - h[i].free_cpu;
+        int32_t  used_gpu  = h[i].total_gpu - h[i].free_gpu;
 
-        fmt_mb(h[i].total_mem_mb, tmp, sizeof(tmp));
-        w->mem = imax(w->mem, (int) strlen(tmp));
-        fmt_mb(used_mem, tmp, sizeof(tmp));
-        w->used_mem = imax(w->used_mem, (int) strlen(tmp));
-        fmt_mb(h[i].total_storage_mb, tmp, sizeof(tmp));
-        w->storage = imax(w->storage, (int) strlen(tmp));
-        fmt_mb(used_stor, tmp, sizeof(tmp));
-        w->used_storage = imax(w->used_storage, (int) strlen(tmp));
-        w->name = imax(w->name, (int) strlen(h[i].name));
-        w->state = imax(w->state, (int) strlen(host_state_str(h[i].state)));
-        w->max = imax(w->max, ndigits(h[i].max_jobs));
+        fmt_mem(h[i].total_mem_mb, tmp, sizeof(tmp));
+        w->mem = imax(w->mem, (int)strlen(tmp));
+        fmt_mem(used_mem, tmp, sizeof(tmp));
+        w->used_mem = imax(w->used_mem, (int)strlen(tmp));
+        fmt_stor(h[i].total_storage_mb, tmp, sizeof(tmp));
+        w->storage = imax(w->storage, (int)strlen(tmp));
+        fmt_stor(used_stor, tmp, sizeof(tmp));
+        w->used_storage = imax(w->used_storage, (int)strlen(tmp));
+
+        w->name      = imax(w->name,      (int)strlen(h[i].name));
+        w->state     = imax(w->state,     (int)strlen(host_state_str(h[i].state)));
+        w->max       = imax(w->max,       ndigits(h[i].max_jobs));
         w->total_cpu = imax(w->total_cpu, ndigits(h[i].total_cpu));
-        w->used_cpu = imax(w->used_cpu, ndigits(used_cpu));
+        w->used_cpu  = imax(w->used_cpu,  ndigits(used_cpu));
         w->total_gpu = imax(w->total_gpu, ndigits(h[i].total_gpu));
-        w->used_gpu = imax(w->used_gpu, ndigits(used_gpu));
-        w->njobs = imax(w->njobs, ndigits(h[i].num_jobs));
-        w->run = imax(w->run, ndigits(h[i].num_run));
-        w->susp = imax(w->susp, ndigits(h[i].num_susp));
+        w->used_gpu  = imax(w->used_gpu,  ndigits(used_gpu));
+        w->njobs     = imax(w->njobs,     ndigits(h[i].num_jobs));
+        w->run       = imax(w->run,       ndigits(h[i].num_run));
+        w->susp      = imax(w->susp,      ndigits(h[i].num_susp));
     }
 }
 
@@ -131,17 +138,19 @@ static void usage(void)
                     "  --version      output version information and exit\n");
 }
 
-static struct option longopts[] = {{"help", no_argument, NULL, 'h'},
-                                   {"version", no_argument, NULL, 'V'},
-                                   {"close", required_argument, NULL, 'c'},
-                                   {"open", required_argument, NULL, 'o'},
-                                   {NULL, 0, NULL, 0}};
+static struct option longopts[] = {
+    { "help",    no_argument,       NULL, 'h' },
+    { "version", no_argument,       NULL, 'V' },
+    { "close",   required_argument, NULL, 'c' },
+    { "open",    required_argument, NULL, 'o' },
+    { NULL, 0, NULL, 0 }
+};
 
 int main(int argc, char **argv)
 {
     int cc;
     const char *close_host = NULL;
-    const char *open_host = NULL;
+    const char *open_host  = NULL;
 
     while ((cc = getopt_long(argc, argv, "c:o:hV", longopts, NULL)) != EOF) {
         switch (cc) {
@@ -187,42 +196,51 @@ int main(int argc, char **argv)
     }
 
     struct col_widths w;
+    char mem_buf[FMT_BUF_LEN];
+    char stor_buf[FMT_BUF_LEN];
+
     compute_widths(hosts, nhosts, &w);
 
-    printf("%-*s  %-*s  %*s  %*s  %*s  %*s  %*s  %*s  %*s  %*s  %*s  %*s  %*s  "
-           "%*s\n",
-           w.name, "HOST_NAME", w.state, "STATE", w.max, "MAX", w.total_cpu,
-           "NCPU", w.mem, "MEM", w.storage, "STOR", w.total_gpu, "NGPU",
-           w.njobs, "NJOBS", w.run, "RUN", w.susp, "SUSP", w.used_cpu,
-           "USED_CPU", w.used_mem, "USED_MEM", w.used_storage, "USED_STOR",
-           w.used_gpu, "USED_GPU");
-
-    char mem_buf[FMT_BUF_LEN];
-    char used_mem_buf[FMT_BUF_LEN];
-    char stor_buf[FMT_BUF_LEN];
-    char used_stor_buf[FMT_BUF_LEN];
+    printf("%-*s  %-*s  %*s  %*s  %*s  %*s  %*s  %*s  %*s  %*s  %*s  %*s  %*s  %*s\n",
+           w.name,         "HOST_NAME",
+           w.state,        "STATE",
+           w.max,          "MAX",
+           w.total_cpu,    "NCPU",
+           w.mem,          "MEM_MB",
+           w.storage,      "STOR_TB",
+           w.total_gpu,    "NGPU",
+           w.njobs,        "NJOBS",
+           w.run,          "RUN",
+           w.susp,         "SUSP",
+           w.used_cpu,     "USED_CPU",
+           w.used_mem,     "USED_MB",
+           w.used_storage, "USED_TB",
+           w.used_gpu,     "USED_GPU");
 
     for (int i = 0; i < nhosts; i++) {
-        uint64_t used_mem = hosts[i].total_mem_mb - hosts[i].free_mem_mb;
-        uint64_t used_stor =
-            hosts[i].total_storage_mb - hosts[i].free_storage_mb;
-        int32_t used_cpu = hosts[i].total_cpu - hosts[i].free_cpu;
-        int32_t used_gpu = hosts[i].total_gpu - hosts[i].free_gpu;
+        uint64_t used_mem  = hosts[i].total_mem_mb - hosts[i].free_mem_mb;
+        uint64_t used_stor = hosts[i].total_storage_mb - hosts[i].free_storage_mb;
+        int32_t  used_cpu  = hosts[i].total_cpu - hosts[i].free_cpu;
+        int32_t  used_gpu  = hosts[i].total_gpu - hosts[i].free_gpu;
 
-        fmt_mb(hosts[i].total_mem_mb, mem_buf, sizeof(mem_buf));
-        fmt_mb(used_mem, used_mem_buf, sizeof(used_mem_buf));
-        fmt_mb(hosts[i].total_storage_mb, stor_buf, sizeof(stor_buf));
-        fmt_mb(used_stor, used_stor_buf, sizeof(used_stor_buf));
+        fmt_mem(hosts[i].total_mem_mb, mem_buf, sizeof(mem_buf));
+        fmt_stor(hosts[i].total_storage_mb, stor_buf, sizeof(stor_buf));
 
-        printf("%-*s  %-*s  %*d  %*d  %*s  %*s  %*d  %*d  %*d  %*d  %*d  %*s  "
-               "%*s  %*d\n",
-               w.name, hosts[i].name, w.state, host_state_str(hosts[i].state),
-               w.max, hosts[i].max_jobs, w.total_cpu, hosts[i].total_cpu, w.mem,
-               mem_buf, w.storage, stor_buf, w.total_gpu, hosts[i].total_gpu,
-               w.njobs, hosts[i].num_jobs, w.run, hosts[i].num_run, w.susp,
-               hosts[i].num_susp, w.used_cpu, used_cpu, w.used_mem,
-               used_mem_buf, w.used_storage, used_stor_buf, w.used_gpu,
-               used_gpu);
+        printf("%-*s  %-*s  %*d  %*d  %*s  %*s  %*d  %*d  %*d  %*d  %*d  %*lu  %*lu  %*d\n",
+               w.name,         hosts[i].name,
+               w.state,        host_state_str(hosts[i].state),
+               w.max,          hosts[i].max_jobs,
+               w.total_cpu,    hosts[i].total_cpu,
+               w.mem,          mem_buf,
+               w.storage,      stor_buf,
+               w.total_gpu,    hosts[i].total_gpu,
+               w.njobs,        hosts[i].num_jobs,
+               w.run,          hosts[i].num_run,
+               w.susp,         hosts[i].num_susp,
+               w.used_cpu,     used_cpu,
+               w.used_mem,     used_mem,
+               w.used_storage, used_stor,
+               w.used_gpu,     used_gpu);
     }
 
     llb_free_host_info(hosts, nhosts);
